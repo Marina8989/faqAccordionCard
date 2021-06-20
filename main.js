d044abe4e4819ead5af6fb9b30c3c9ecbc753c1b
Marina8989 <marina.mill209@gmail.com>
2021-06-19 20:21:39 -0700

    added all js now onto queries

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 const pars = document.querySelectorAll('p');
-const arrows = document.querySelectorAll('h3 img');
+const arrows = document.querySelectorAll('.itemImg');
+
+const text = [
+    {
+        p: 'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.'
+    },
+    {
+        p: 'No more than 2GB. All files in your account must fit your allotted storage space.'
+    },
+    {
+        p: 'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.'
+    },
+    {
+        p: 'Yes! Send us a message and we’ll process your request no questions asked.'
+    },
+    {
+        p: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.'
+    },
+]
+
+
+function logText(e) {
+   console.log(e.value);
+}
 
 arrows.forEach(arrow => {
-    arrow.addEventListener('click', () => {
-        pars.forEach(par => {
-            par.classList.toggle('active');
-        })
+    arrow.addEventListener('click', (e) => {
+        let itemEl = e.target.parentElement.parentElement;
+        console.log(itemEl);
+        itemEl.classList.toggle('active');
     })
 })
